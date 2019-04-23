5e7f9f57e8e21d5aa329d8b21879db2f7294fa3d
mustafashaikh4714 <shaikhmustafa.4714@gmail.com>
2019-04-24 00:57:03 +0530

    add user to redis

--- app.js
+++ app.js
@@ -2,13 +2,9 @@ const express = require('express')
 const hbs = require('hbs')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const redis = require('redis')
 
 const PORT = process.env.PORT || 3000
 const app = express()
-const client = redis.createClient()
-
-client.on('connect', () => console.log('connected to redis'))
 
 // APP MIDDLEWARE
 app.use(methodOverride('_method'))
@@ -20,7 +16,7 @@ app.use(
   })
 )
 
-require('/routes/index')(app)
+require('./routes/index')(app)
 
 app.listen(PORT, () => console.log(`server started at ${PORT}`))
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,27 @@
+const redis = require('redis')
 module.exports = app => {
+  // Setup redis client.
+  const client = redis.createClient()
+  client.on('connect', () => console.log('connected to redis'))
+
   app.get('/', (req, res) => {
-    res.send('redis datastructure store demo.')
+    res.send('Redis Demo.')
+  })
+
+  app.get('/add/user', (req, res) => {
+    res.render('add_user')
+  })
+
+  app.post('/add/user', (req, res) => {
+    let { username, email, phone } = req.body
+    client.hmset(
+      username,
+      ['email', email, 'phone', phone],
+      (err, response) => {
+        if (err) console.log(err)
+        console.log(response)
+        res.redirect('/')
+      }
+    )
   })
 }
